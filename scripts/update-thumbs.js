35f79bc35016f1e2fdf604571bc886ddcf605f3e
LiosxD <36986527+LiosxD@users.noreply.github.com>
2023-08-02 23:36:26 -0600

    Create update-thumbs.js

---
+++ scripts/update-thumbs.js
@@ -0,0 +1,19 @@
+// run this as a macro to sync thumbnail images into the thumbnails folder
+const moduleId = 'extinction-curse-pf2e-maps-by-lios'
+const thumbnailFolder = 'assets/thumbnails'
+
+for (const pack of game.packs.filter(p => p.metadata.packageName === moduleId & p.metadata.type === 'Scene')) {
+    for (const scene of pack.index.values()) {
+        const blob = await fetch(scene.thumb)
+            .then(r => r.blob())
+        const {path} = await FilePicker.upload(
+            'data',
+            `modules/${moduleId}/${thumbnailFolder}/`,
+            new File([blob], scene.thumb.split('/').pop(), {type: blob.type}),
+            {},
+            {notify: false}
+        );
+        await pack.getDocument(scene._id)
+            .then(s => s.update({thumb: path}))
+    }
+}
